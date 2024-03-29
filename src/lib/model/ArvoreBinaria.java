777440959bcaeb32332ba8a81390877dc849f32a
micaelygusmao <micaely.gusmao@estudante.ifes.edu.br>
2024-03-29 10:42:46 -0300

    Implementação dos métodos adicionar, quantidade e altura

--- src/lib/model/ArvoreBinaria.java
+++ src/lib/model/ArvoreBinaria.java
@@ -14,7 +14,29 @@ public class ArvoreBinaria<T> implements IArvoreBinaria<T> {
 
     @Override
     public void adicionar(T novoValor) {
+        No<T> novoNo = new No<T>(novoValor);
 
+        if (this.raiz == null) {
+            this.raiz = novoNo;
+        } else {
+            setPosicaoENovoValor(this.raiz, novoValor);
+        }
+    }
+
+    public void setPosicaoENovoValor(No<T> no, T novoValor){
+        if(comparador.compare(novoValor, no.getValor()) < 0){
+            if(no.getFilhoEsquerda() == null){
+                no.setFilhoEsquerda(new No<>(novoValor));
+            } else {
+                setPosicaoENovoValor(no.getFilhoEsquerda(), novoValor);
+            }
+        } else {
+            if(no.getFilhoDireita() == null){
+                no.setFilhoDireita(new No<>(novoValor));
+            } else {
+                setPosicaoENovoValor(no.getFilhoDireita(), novoValor);
+            }
+        }
     }
 
     @Override
@@ -34,12 +56,43 @@ public class ArvoreBinaria<T> implements IArvoreBinaria<T> {
 
     @Override
     public int altura() {
-        return 0;
+        int alturaEsquerda = AlturaDaEsquerda(this.raiz, 0);
+        int alturaDireita = AlturaDaDireita(this.raiz, 0);
+        return 1 + Math.max(alturaEsquerda, alturaDireita);
     }
+
+    public int AlturaDaEsquerda(No<T> no, int contd){
+        if(no == null) return -1;
+
+        if(no.getFilhoEsquerda() != null){
+            contd = AlturaDaEsquerda(no.getFilhoEsquerda(), contd + 1);
+        }
+
+        return contd;
+    }
+
+    public int AlturaDaDireita(No<T> no, int contd){
+        if(no == null) return -1;
+
+        if(no.getFilhoDireita() != null){
+            contd = AlturaDaDireita(no.getFilhoDireita(), contd + 1);
+        }
+
+        return contd;
+    }
+
 
     @Override
     public int quantidadeNos() {
-        return 0;
+        return percorreTodaArvore(this.raiz);
+    }
+
+    private int percorreTodaArvore(No<T> no) {
+        if (no == null) {
+            return 0;
+        }
+
+        return 1 + percorreTodaArvore(no.getFilhoEsquerda()) + percorreTodaArvore(no.getFilhoDireita());
     }
 
     @Override
