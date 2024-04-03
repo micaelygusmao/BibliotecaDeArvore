2fa7d8f18e196212f361c3e1a884e19cf8d92270
micaelygusmao <micaely.gusmao@estudante.ifes.edu.br>
2024-04-02 21:14:52 -0300

    Adição de métodos de verificação especificos ao nó

--- src/lib/model/No.java
+++ src/lib/model/No.java
@@ -53,4 +53,52 @@ public class No<T> {
     public void setFilhoEsquerda(No<T> filhoEsquerda) {
         this.filhoEsquerda = filhoEsquerda;
     }
+
+    /**
+     * Método para verificar se o nó é uma folha
+     * @return caso tenha filhos a esquerda e a direita o retorno é false, se não tiver o retorno é true.
+     */
+     boolean NoIsFolha(){
+        if(this.NoTemFilhoAEsquerda() && this.NoTemFilhoADireita()) return true;
+
+        return false;
+    }
+
+    /**
+     * Método para verificar se o nó tem filho a esquerda
+     * @return caso tenha filhos a esquerda o retorno é true, se não tiver o retorno é false.
+     */
+    boolean NoTemFilhoAEsquerda(){
+        if(this.getFilhoEsquerda() != null) return true;
+
+        return false;
+    }
+
+    /**
+     * Método para verificar se o nó tem filho a direita
+     * @return caso tenha filhos a direta o retorno é true, se não tiver o retorno é false.
+     */
+    boolean NoTemFilhoADireita(){
+        if(this.getFilhoDireita() != null) return true;
+
+        return false;
+    }
+
+    /**
+     * Método para verificar se o nó tem apenas um filho
+     * @return caso tenha um filho o retorno é true, se não tiver o retorno é false.
+     */
+    boolean NoTemApenasUmFilho(){
+        if((this.NoTemFilhoAEsquerda() && !this.NoTemFilhoADireita()) ||
+                (this.NoTemFilhoADireita() && !this.NoTemFilhoAEsquerda())){
+            return true;
+        }
+
+        return false;
+    }
+
+    No<T> GetNoPaiByFilho() {
+
+        return null;
+    }
 }
